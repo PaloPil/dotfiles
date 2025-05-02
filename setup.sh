aff8df0c35f1870672d8725f8f8bbf7c43461661
paul.rabiet-steichen <71895868+PaloPil@users.noreply.github.com>
2025-05-02 16:29:44 +0200

    add: Fish shortcuts to setup.sh

--- setup.sh
+++ setup.sh
@@ -7,5 +7,9 @@ for folder in "$path"/config/*; do
   ./lib/safe_symlink.sh "$HOME/.config/$(basename "$folder")" "$folder"
 done
 
+# FISH SHORTCUTS
+./lib/safe_symlink.sh "$HOME/.fishrc" "$HOME/.config/fish/config.fish"
+./lib/safe_symlink.sh "$HOME/.fish_aliases" "$HOME/.config/fish/conf.d/aliases.fish"
+
 # KEYD CONFIGURATION
 sudo ./lib/safe_symlink.sh "/etc/keyd" "$HOME/.config/keyd"
