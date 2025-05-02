9f96e356953d0bf7bba4e59739908219e5fb8397
paul.rabiet-steichen <71895868+PaloPil@users.noreply.github.com>
2025-05-02 13:55:22 +0200

    fix: Make setup.sh remove old files before symlink

--- setup.sh
+++ setup.sh
@@ -5,10 +5,12 @@ cd config || (echo "'config' folder not found"; exit)
 current_dir=$(pwd)
 
 for folder in *; do
+  rm -rf "$HOME/.config/$folder"
   ln -s "$current_dir/$folder" "$HOME/.config/$folder"
 done
 
 cd ..
 
 # KEYD CONFIGURATION
+rm -rf "/etc/keyd"
 sudo ln -s "$HOME/.config/keyd" "/etc/keyd"
