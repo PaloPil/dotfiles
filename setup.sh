c282a4afc7d0b94ae43d006529c852805a5099b0
paul.rabiet-steichen <71895868+PaloPil@users.noreply.github.com>
2025-05-02 14:34:33 +0200

    fix: no cd in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,14 +1,11 @@
 #!/usr/bin/env bash
 
-cd config || (echo "'config' folder not found"; exit)
+config_dir="$(pwd)/config"
+dotfiles=$(ls "$config_dir")
 
-config_dir=$(pwd)
-
-for folder in *; do
+for folder in $dotfiles; do
   ./lib/safe_symlink.sh "$HOME/.config/$folder" "$config_dir/$folder"
 done
-
-cd ..
 
 # KEYD CONFIGURATION
 sudo ./lib/safe_symlink.sh "/etc/keyd" "$HOME/.config/keyd"
