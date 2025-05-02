b4928eeb90e7fd138cd8f1a9e857b633f0f3f1d9
paul.rabiet-steichen <71895868+PaloPil@users.noreply.github.com>
2025-05-02 17:14:10 +0200

    fix(tmux): Fixed fish path

--- setup.sh
+++ setup.sh
@@ -15,6 +15,9 @@ done
 # FISH SHORTCUTS
 ./lib/safe_symlink.sh "$HOME/.fishrc" "$HOME/.config/fish/config.fish"
 ./lib/safe_symlink.sh "$HOME/.fish_aliases" "$HOME/.config/fish/conf.d/aliases.fish"
+if [ ! -e "/usr/bin/fish" ]; then
+  ln -s "$(which fish)" "/usr/bin/fish"
+fi
 
 # KEYD CONFIGURATION
 sudo ./lib/safe_symlink.sh "/etc/keyd" "$HOME/.config/keyd"
