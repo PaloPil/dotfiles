fda56c7304fff3d774d8a3d34dce6a7f0e7a122b
paul.rabiet-steichen <71895868+PaloPil@users.noreply.github.com>
2025-05-02 16:36:20 +0200

    add: dotfiles folder ability

--- setup.sh
+++ setup.sh
@@ -7,6 +7,11 @@ for folder in "$path"/config/*; do
   ./lib/safe_symlink.sh "$HOME/.config/$(basename "$folder")" "$folder"
 done
 
+# DOTFILES
+for file in "$path"/dotfiles/*; do
+  ./lib/safe_symlink.sh "$HOME/.$(basename "$file")" "$file"
+done
+
 # FISH SHORTCUTS
 ./lib/safe_symlink.sh "$HOME/.fishrc" "$HOME/.config/fish/config.fish"
 ./lib/safe_symlink.sh "$HOME/.fish_aliases" "$HOME/.config/fish/conf.d/aliases.fish"
