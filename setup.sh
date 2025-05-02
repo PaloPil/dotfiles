0395502a77bea8ffe2909a00a28530b78ab24a7f
paul.rabiet-steichen <71895868+PaloPil@users.noreply.github.com>
2025-05-02 13:38:29 +0200

    Edit setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-cp "config" "$HOME/.config"
+cd config || (echo "'config' folder not found"; exit)
+
+current_dir=$(pwd)
+
+for folder in *; do
+  ln -s "$current_dir/$folder" "$HOME/.config/$folder"
+done
+
+cd ..
 
 # KEYD CONFIGURATION
 sudo ln -s "$HOME/.config/keyd" "/etc/keyd"
